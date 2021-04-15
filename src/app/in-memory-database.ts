da4f3dbab1cd5fd5a893bf445ead65a7abe2abd0
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-04-14 21:31:58 -0300

    intercepting request with angular-in-memory-web-api

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,6 @@
 import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
 import { Observable } from 'rxjs';
-import { Category } from './pages/categories/shared/category.model'
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
