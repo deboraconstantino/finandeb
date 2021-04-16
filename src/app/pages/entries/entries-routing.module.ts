c7fafe021ba6cb7762cb952eab684ce6375c0a02
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-04-15 22:12:43 -0300

    adding EntryListComponent

--- src/app/pages/entries/entries-routing.module.ts
+++ src/app/pages/entries/entries-routing.module.ts
@@ -1,7 +1,10 @@
+import { EntryListComponent } from './entry-list/entry-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EntryListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
