fb8cd152293a37d870dd255241a1eea587cb1cd0
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-05-10 20:23:35 -0300

    adding BaseResourceListComponent

---
+++ src/app/shared/components/base-resource-list/base-resource-list/base-resource-list.component.ts
@@ -0,0 +1,34 @@
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import { Component, OnInit } from '@angular/core';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+@Component({
+  selector: 'app-base-resource-list',
+  templateUrl: './base-resource-list.component.html',
+  styleUrls: ['./base-resource-list.component.css']
+})
+export class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
+  resources: T[] = [];
+
+  constructor(
+    private resourceService: BaseResourceService<T>
+  ) { }
+
+  ngOnInit(): void {
+    this.resourceService.getAll().subscribe(
+      resource => this.resources = resource.sort((a, b) => b.id - a.id),
+      error => alert('Erro ao carregar a lista de lançamentos: ' + error)
+    );
+  }
+
+  deleteResource(resource: T): void {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.resourceService.delete(resource.id).subscribe(
+        () => this.resources = this.resources.filter(element => element !== resource),
+        error => alert('Erro ao tentar excluir: ' + error)
+      );
+    }
+  }
+}
